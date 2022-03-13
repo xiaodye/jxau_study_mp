a0999cb8004530ce8ce7e9a70d58e921cc4f9e48
lansen <bluewoods.lan@gmail.com>
2022-03-13 22:33:11 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <script>
 export default {
+  globalData: {
+    statusBarHeight: 0, // 状态导航栏高度
+    windowHeight: 0, // 可使用窗口高度
+    navHeight: 0,
+    navigationBarHeight: 0, // 导航栏高度(标题栏高度)
+  },
   onLaunch: function () {
     console.log("App Launch");
+
+    // 状态栏高度
+    this.globalData.statusBarHeight = uni.getSystemInfoSync()["statusBarHeight"];
+    // 可使用窗口高度
+    this.globalData.windowHeight = uni.getSystemInfoSync()["windowHeight"];
+
+    // #ifdef MP-WEIXIN
+    // 获取微信胶囊的位置信息 width,height,top,right,left,buttom
+    const custom = wx.getMenuButtonBoundingClientRect();
+    // console.log(custom)
+
+    // 总体高度 = 状态栏高度 + 胶囊高度 + （胶囊距离顶部的距离 - 状态栏高度）* 2
+    const navHeight = this.globalData.statusBarHeight + custom.height + (custom.top - this.globalData.statusBarHeight) * 2;
+    this.globalData.navHeight = navHeight;
+
+    // 导航栏高度(标题栏高度) = 总体高度 - 状态栏高度
+    this.globalData.navigationBarHeight = navHeight - this.globalData.statusBarHeight;
+    // console.log("导航栏高度："+this.globalData.navigationBarHeight)
+    // #endif
+
+    console.log(this.globalData);
   },
   onShow: function () {
     console.log("App Show");
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,46 +1,139 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view>
-      <text class="title">{{ title }}</text>
+  <view class="home" :style="{ paddingTop: statusBarHeight + 'px', minHeight: windowHeight + 'px' }">
+    <!-- 导航栏(标题栏) -->
+    <view class="nav" :style="{ height: navigationBarHeight + 'px' }">
+      <u-search placeholder="前端" :showAction="false" :disabled="true"></u-search>
     </view>
-    <u-button type="primary" text="uni-vscode-template"></u-button>
+
+    <!-- tab栏 -->
+    <view class="tab" :style="{ top: navHeight + 'px' }">
+      <u-tabs
+        :scrollable="false"
+        :list="navList"
+        lineWidth="30"
+        lineHeight="5"
+        lineColor="#f56c6c"
+        :activeStyle="{
+          color: '#303133',
+          fontWeight: 'bold',
+          fontSize: 20 + 'px',
+          transform: 'scale(1.05)',
+        }"
+        :inactiveStyle="{
+          color: '#606266',
+          fontSize: 17 + 'px',
+          transform: 'scale(1)',
+        }"
+        itemStyle="padding-left: 15px; padding-right: 15px; height: 34px;"
+      ></u-tabs>
+
+      <!-- 子tab -->
+      <view class="sub-tab">
+        <u-tag text="推荐" type="warning" shape="circle" size="mini" bgColor="#71d5a1" color="#333" borderColor="#fff"></u-tag>
+        <u-tag text="最热" type="success" shape="circle" size="mini" bgColor="#f4f4f5" color="#333" borderColor="#fff"></u-tag>
+        <u-tag text="最新" type="success" shape="circle" size="mini" bgColor="#f4f4f5" color="#333" borderColor="#fff"></u-tag>
+      </view>
+    </view>
+
+    <!-- 文章列表 -->
+    <view class="ariticle" :style="{ paddingTop: 80 + 'px' }">
+      <u-search placeholder="前端" :showAction="false"></u-search>
+      <u-search placeholder="前端" :showAction="false"></u-search>
+      <u-search placeholder="前端" :showAction="false"></u-search>
+    </view>
   </view>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      title: "Hello",
-    };
+  components: {},
+  data: () => ({
+    statusBarHeight: 0,
+    navigationBarHeight: 0,
+    navHeight: 0,
+    windowHeight: 0,
+
+    navList: [
+      {
+        name: "文章",
+        badge: {
+          isDot: true,
+        },
+      },
+      {
+        name: "讨论",
+      },
+      {
+        name: "视频",
+      },
+      {
+        name: "资料",
+      },
+    ],
+  }),
+
+  methods: {
+    // 得到状态导航栏高度
+    getSystemInfo() {
+      this.statusBarHeight = getApp().globalData.statusBarHeight;
+      this.navigationBarHeight = getApp().globalData.navigationBarHeight;
+      this.windowHeight = getApp().globalData.windowHeight;
+      this.navHeight = getApp().globalData.navHeight;
+
+      // console.log(this.statusBarHeight)
+      // console.log(this.windowHeight)
+      // console.log(this.navigationBarHeight)
+    },
   },
-  onLoad() {},
-  methods: {},
+
+  onLoad() {
+    this.getSystemInfo();
+  },
 };
 </script>
 
-<style>
-.content {
+<style lang="scss">
+$tab: 160rpx;
+
+.home {
+  background-color: $uni-bg-color-grey;
+  overflow-y: auto;
+  box-sizing: border-box;
+}
+
+.nav {
+  // height: 102rpx;
+  padding: 0 240rpx 0 40rpx;
+  // padding: 10rpx 240rpx 10rpx 40rpx;
   display: flex;
-  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  justify-content: center;
 }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin: 200rpx auto 50rpx auto;
+.tab {
+  background-color: #ffffff;
+  height: $tab;
+  // padding-bottom: 20rpx;
+  position: absolute;
+  width: 100%;
+  .u-tabs {
+    height: 100rpx;
+    display: flex;
+    justify-content: center;
+    // align-items: center;
+  }
+  .sub-tab {
+    padding-left: 30rpx;
+    display: flex;
+    .u-tag {
+      margin-right: 20rpx !important;
+      padding: 0 20rpx;
+      // font-size: 24rpx;
+    }
+  }
 }
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+.ariticle {
+  margin-top: 40rpx;
 }
 </style>
